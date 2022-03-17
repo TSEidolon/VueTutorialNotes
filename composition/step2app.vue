ec72e6b9a29ac7de115f43cb62834d17be7e282f
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-17 00:17:58 -0600

    tutorial step 2 composition

---
+++ composition/step2app.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { reactive } from 'vue'
+import {ref} from 'vue'
+
+  const state = reactive ({
+    count: 0
+  })
+  function increment(){
+    state.count++
+  }
+  
+  const message = ref('Hello World!')
+// component logic
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <p> count is: {{ state.count }}</p>
+		<button @click="increment"> {{ state.count }}</button>
+  </div>
+</template>
