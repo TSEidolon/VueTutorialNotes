3c312808482a11b814a92c3d53d5c9531b88a2dd
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-16 23:44:24 -0600

    tutorial step 2

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1> {{message}}</h1> 
+    <h1> {{message.split('').reverse().join("")}}</h1> <!-- split ('') breaks the string into pieces of 1 char. .reverse() reverses the split string. join('') joins the broken string -->
+    <!-- !dlroW olleH -->
+  </div>
+</template>
