1ce1c2f403219c3a594b8cb7092bf2e56d91d02b
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-17 00:19:56 -0600

    tutorial step 3 composition

---
+++ composition/step3app.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue' //import ref is "import"-tant Haaa
+
+const titleClass = ref('title')
+</script>
+
+<template>
+  <h1 :class="titleClass">Make me red</h1> 
+</template>
+
+<style>
+.title {
+  color: red;
+}
+</style>
