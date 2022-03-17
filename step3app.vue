5334562a88e7c084b401c662262724abbe96f777
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-16 23:55:56 -0600

    tutorial step 3

---
+++ step3app.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data () {
+    return {
+      titleClass: 'title' // returns the style color for whatever title means
+    }
+  }
+}
+</script>
+
+<template>
+  <!--v binds the style linked to titleClass through the .title class below in <style>  -->
+  <h1 :class="titleClass" > Make me red </h1>
+</template>
+
+<style>
+  .title {
+    color:red;
+  }
+</style>
+// 
