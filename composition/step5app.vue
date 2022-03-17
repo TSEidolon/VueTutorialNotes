197bb4c7dfa0f05654076d44e6c5190c51ac5210
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-17 00:40:45 -0600

    tutorial step 5 composition

--- composition/step5app.vue
+++ composition/step5app.vue
@@ -1,2 +1,14 @@
 
 
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('')
+</script>
+
+<template>
+<div>
+  <input v-model="text" placeholder="Type here">
+  <p>{{ text }}</p>
+</div>
+</template>
