0bc3f21a315adafe615f84658ccb5093e6942396
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-17 00:01:03 -0600

    tutorial step 4

---
+++ step4app.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++ 
+      //The component instance exposes the data properties declared by data.
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- uses v-on shortcut @ -->
+  <!--  -->
+  <button @click="increment">count is: {{ count }}</button>
+</template>
